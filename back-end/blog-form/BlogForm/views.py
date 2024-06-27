155e7669abb1b6905eece96775dd45d6b8c049f3
gcancel <gmartin9892@gmail.com>
2024-06-26 21:43:05 -0500

    updating ContactInfoForm view with throttling

--- back-end/blog-form/BlogForm/views.py
+++ back-end/blog-form/BlogForm/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 
 from .models import ContactInfo
 from .serializers import ContactInfoSerializer
 
 # Create your views here.
-class ContactInfoForm(generics.GenericAPIView):
+class ContactInfoForm(generics.CreateAPIView):
     queryset = ContactInfo.objects.all()
     serializer_class = ContactInfoSerializer
-    
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
