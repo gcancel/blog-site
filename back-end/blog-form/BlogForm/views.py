8ce30aa2949b89054032cf4cb988c465d9a9207e
gcancel <gmartin9892@gmail.com>
2024-07-14 10:58:34 -0500

    removing unneeded imports and variables, etc.

--- back-end/blog-form/BlogForm/views.py
+++ back-end/blog-form/BlogForm/views.py
@@ -4,20 +4,15 @@ from django.http import HttpResponseRedirect
 from rest_framework.response import Response
 
 # decorators for function based views...
-from rest_framework.decorators import api_view, throttle_classes, permission_classes
+from rest_framework.decorators import api_view, throttle_classes
 
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 
-# from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
-from django.utils.decorators import method_decorator
-
 # import of models, serializers, forms
 from .models import ContactInfo
 from .serializers import ContactInfoSerializer
-from .forms import ContactForm
-
 
 # @method_decorator(csrf_protect, name='dispatch')
 @throttle_classes([UserRateThrottle, AnonRateThrottle])
@@ -28,7 +23,7 @@ def contact_form(request):
         if form.is_valid():
                 form.save()
         # Redirect back to page after successful POST
-        return HttpResponseRedirect(redirect_to='http://localhost:3000/contact-me')
+        return HttpResponseRedirect({'message':'201 created.'}, redirect_to='http://localhost:3000/contact-me')
 
 
 class ContactMessageView(generics.ListAPIView):
